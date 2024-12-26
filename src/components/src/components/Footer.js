79dd31848c34c63d70b8a93a12446025958bdcec
Shama Parveen <139287494+shamap01@users.noreply.github.com>
2024-12-27 05:00:05 +0530

    Create Footer.js
    src/components/Footer.js

---
+++ src/components/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer>
+            <p>&copy; 2023 Article Summarizer</p>
+        </footer>
+    );
+};
+
+export default Footer;
